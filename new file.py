266ae0bd901b29e0fb6d456114d639b8c5bcaa5b
extractg <98511780+extractg@users.noreply.github.com>
2022-01-27 10:26:07 +0200

    Update new file.py

--- new file.py
+++ new file.py
@@ -10,5 +10,8 @@
 </tr>
 <td>Row 2, column 1</td>
 <td>Row 2, column 2</td>
+<a href="https://unsplash.com">
+  <img itemprop="image" style="height: 90px;" src="https://bit.ly/fcc-relaxing-cat" alt="A cute orange cat lying on its back.">
+</a>
 </tbody>
 </table>
